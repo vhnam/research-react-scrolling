bfff39a93c9690a914b958a55548af03ff711228
Nam Vo <vhnam2504@gmail.com>
2018-12-13 21:25:06 +0700

    add filter

--- employee-service/services/employee.js
+++ employee-service/services/employee.js
@@ -7,12 +7,33 @@ module.exports = {
     return new Promise((resolve, reject) => {
       const limit = parseInt(config.API_LIMIT);
       let offset = 0;
+      let condition = {};
 
-      Employee.count().then(pagination => {
+      if (undefined !== query.first_name) {
+        condition.first_name = {
+          $like: `%${query.first_name}%`
+        };
+      }
+
+      if (undefined !== query.last_name) {
+        condition.last_name = {
+          $like: `%${query.last_name}%`
+        };
+      }
+
+      if (undefined !== query.gender_name) {
+        condition.gender_name = query.gender;
+      }
+
+      Employee.count({
+        where: condition
+      })
+      .then(pagination => {
         const pages = Math.ceil(pagination / limit);
         offset = limit * (query.page - 1);
 
         Employee.findAll({
+          where: condition,
           limit: limit,
           offset: offset
         })
