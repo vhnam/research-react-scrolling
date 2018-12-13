c525b74969eca27da8f115df2f94d56007e04314
Nam Vo <vhnam2504@gmail.com>
2018-12-13 19:37:12 +0700

    Initial commit

---
+++ employee-service/config/config.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+
+    API_PORT: process.env.API_PORT,
+    API_VERSION: process.env.API_VERSION,
+  },
+
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  }
+};
