3993e0276dc79e99aead8033c539b5e40fae759b
Nam Vo <vhnam2504@gmail.com>
2018-12-16 02:45:31 +0700

    update filter

--- employee-service/controllers/v1/employee.js
+++ employee-service/controllers/v1/employee.js
@@ -9,6 +9,7 @@ module.exports = {
         first_name: req.query.first_name,
         last_name: req.query.last_name,
         gender: req.query.gender,
+        hiring_year: req.query.hiring_year,
         page: (req.query.page) ? req.query.page : 1
       };
 
@@ -16,6 +17,7 @@ module.exports = {
 
       res.status(200).json(employees);
     } catch (err) {
+      console.log(err.stack);
       logger.error(`[ServiceController] ${err}`);
       res.status(400).json({ message: err.message });
     }
--- employee-service/services/employee.js
+++ employee-service/services/employee.js
@@ -1,58 +1,84 @@
 import { Employee } from '../models';
 
+const sequelize = require('sequelize');
 const config = require('../config');
+
+function filter(query) {
+  let condition = [];
+
+  if (undefined !== query.first_name) {
+    condition.push({
+      first_name: {
+        $like: `%${query.first_name}%`
+      }
+    });
+  }
+
+  if (undefined !== query.last_name) {
+    condition.push({
+      last_name: {
+        $like: `%${query.last_name}%`
+      }
+    });
+  }
+
+  if (undefined !== query.gender) {
+    condition.push({
+      gender: query.gender
+    });
+  }
+
+  if (undefined !== query.hiring_year) {
+    condition.push(
+      sequelize.where(
+        sequelize.fn('YEAR', sequelize.col('hire_date')),
+        query.hiring_year
+      )
+    );
+  }
+
+  return condition;
+}
 
 module.exports = {
   getEmployees: async query => {
     return new Promise((resolve, reject) => {
-      const limit = parseInt(config.API_LIMIT);
       let offset = 0;
-      let condition = {};
-
-      if (undefined !== query.first_name) {
-        condition.first_name = {
-          $like: `%${query.first_name}%`
-        };
-      }
-
-      if (undefined !== query.last_name) {
-        condition.last_name = {
-          $like: `%${query.last_name}%`
-        };
-      }
-
-      if (undefined !== query.gender_name) {
-        condition.gender_name = query.gender;
-      }
+      const limit = parseInt(config.API_LIMIT);
+      const condition = filter(query);
 
       Employee.count({
-        where: condition
+        where: {
+          $and: condition
+        }
       })
-      .then(pagination => {
-        const pages = Math.ceil(pagination / limit);
-        offset = limit * (query.page - 1);
+        .then(pagination => {
+          const pages = Math.ceil(pagination / limit);
+          offset = limit * (query.page - 1);
 
-        Employee.findAll({
-          where: condition,
-          limit: limit,
-          offset: offset
-        })
-        .then(employees => {
-          resolve({
-            data: employees,
-            total: pagination,
-            per_page: limit,
-            last_page: pages,
-            current_page: query.page
-          });
+          return Employee.findAll({
+            where: {
+              $and: condition
+            },
+            limit: limit,
+            offset: offset
+          })
+            .then(employees => {
+              resolve({
+                data: employees,
+                total: pagination,
+                per_page: limit,
+                last_page: pages,
+                current_page: query.page
+              });
+            })
+            .catch(err => {
+              reject(err);
+            });
         })
         .catch(err => {
           reject(err);
         });
-      })
-      .catch(err => {
-        reject(err);
-      });
     });
   }
 };
